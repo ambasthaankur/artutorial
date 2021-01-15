30250260c7edbf2cbb5e0cb543f0b390499c591d
ambasthaankur <68092468+ambasthaankur@users.noreply.github.com>
2021-01-15 20:03:13 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Ar Tutorial</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<!-- background-color #0a183d  menu bd color #08192d   font-family: "Montserrat", sans-serif;
+    -webkit-font-smoothing: antialiased;-->
+<body>
+	<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  		<a class="navbar-brand" href="index.php"><span class="logo_1">AR</span>TUTORIAL</a>
+  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+  		<div class="collapse navbar-collapse" id="collapsibleNavbar">
+    		<ul class="navbar-nav">
+      			<li class="nav-item">
+        			<a class="nav-link" href="index.php">Home</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="courses.php">Courses</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="videos.php">Videos</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="blogs.php">Blogs</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="contact.php">Contact</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="about.php">About Us</a>
+      			</li>    
+    		</ul>
+  		</div>  
+	</nav>
+</body>
+</html>
